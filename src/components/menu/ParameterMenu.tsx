7ff46f9e8299596076751041946663089e8f4896
tclauzure <theo.clauzure@outlook.fr>
2023-10-19 16:32:22 +0200

    menu

---
+++ src/components/menu/ParameterMenu.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./ParameterMenu.module.scss";
+
+interface Props { }
+
+export const ParameterMenu: React.FC<Props> = () => (
+  <div className={styles.container}>
+    
+  </div>
+);
