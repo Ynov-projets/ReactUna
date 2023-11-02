a349359673b7b74d29d47bdda7d3ddba8190b0da
tclauzure <theo.clauzure@outlook.fr>
2023-11-02 14:16:13 +0100

    feature : router + menu + menu parameter

--- src/components/layout/MenuLayout.tsx
+++ src/components/layout/MenuLayout.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./menuLayout.module.scss"
 import { Link, Outlet } from "react-router-dom";
+import logo from "../../assets/images/BigLogo.png"
 
 interface Props { }
 export const MenuLayout: React.FC<Props> = () => (
-  <div className={styles.container}>
+  <div className={styles.container}/*bg-cackground pl-[5px] pt-[15px] */>
     <div>
-      <Link to={"/"} className={styles.logo}></Link>
+      <Link to={"/"} className={styles.logo}/* block w-[270px] h-auto overflow-hidden*/>
+        <img src={logo} alt="logo" /*className="w-full h-auto"*//>
+      </Link>
     </div>
     <div className={styles.tab}>
       <Outlet/>
--- src/components/menu/ParameterMenu.tsx
+++ src/components/menu/ParameterMenu.tsx
@@ -4,7 +4,19 @@ import styles from "./ParameterMenu.module.scss";
 interface Props { }
 
 export const ParameterMenu: React.FC<Props> = () => (
-  <div className={styles.container}>
-    
+  <div className={styles.parameter}>
+    <div className={styles.mode}>
+      MODE
+      <div className={styles.mode__buttons}>
+        <button>NORMAL</button>
+        <button>REVERSE</button>
+      </div>
+    </div>
+    <div className={styles.volume}>
+      VOLUME
+    </div>
+    <div className={styles.close}>
+      <button>FERMER</button>
+    </div>
   </div>
 );
