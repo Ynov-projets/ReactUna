f57402c0f898573b9c0fae061d6f816b0b1f4779
tclauzure <theo.clauzure@outlook.fr>
2023-11-03 14:45:15 +0100

    fix: size of button and menu

--- src/components/layout/MenuLayout.tsx
+++ src/components/layout/MenuLayout.tsx
@@ -4,13 +4,18 @@ import logo from "../../assets/images/BigLogo.png"
 
 interface Props { }
 export const MenuLayout: React.FC<Props> = () => (
-  <div className="bg-background pl-[5px] pt-[15px] pb-[80px] h-screen">
+  <div className="bg-background relative pl-[5px] pt-[15px] pb-[80px] h-screen ">
     <div>
-      <Link to={"/"} className="block w-[270px] h-auto overflow-hidden">
+      <Link to={"/"} className="block w-[180px] h-auto overflow-hidden sm:w-[200px] lg:w-[260px] xl:w-[300px]">
         <img src={logo} alt="logo" className="w-full h-auto"/>
       </Link>
     </div>
-    <div className="flex justify-center bg-white mx-auto max-w-[390px] rounded-xl p-[20px]">
+    <div className=" 
+      relative flex justify-center bg-white mx-auto max-w-[420px] h-[300px] rounded-xl p-[20px] 
+      sm:max-w-[580px] sm:h-[450px]
+      lg:top-[-55px] lg:max-w-[650px]
+      xl:max-w-[750px] xl:h-[500px] xl:top-[-90px]
+    ">
       <Outlet/>
     </div>
   </div>
