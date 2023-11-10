cdc6b7088f22b095312751324b928464dde9026e
zHatsuharu <zhatsuharu@gmail.com>
2023-11-10 16:07:42 +0100

    Add proxy config fixing CORS errors

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://api:3000'
+      }
+    },
   },
   plugins: [react()],
 })
