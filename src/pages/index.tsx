d7b19106fcf88f86f5492cd025075d070507ba9e
tclauzure <theo.clauzure@outlook.fr>
2023-11-15 21:21:30 +0100

    feature: play button and fix: auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,18 @@ interface Props {}
 
 export const HomePage: React.FC<Props> = () => {
   const [open, setIsOpen] = useState(false);
-  // setIsOpen(false);
   
   accountService.isLoggedIn()
   return (
     <>
       <div className="w-full h-screen bg-background">
         {accountService.isLoggedIn() ? (
-        <div className="absolute top-[10px] right-[10px]">
+        <div className="flex items-center gap-[35px] absolute top-[10px] right-[10px] uppercase text-white text-[20px] mr-[50px] hover:shadow-xl">
+              <div className="w-[50px] h-auto"><img className="w-full" src="src\assets\images\pp.png" alt="pic" /></div>
+              <p>Jean michel</p>
+            </div>
+        ) : (
+            <div className="absolute top-[10px] right-[10px]">
           <Link to={"/login"}>
             <button
               className="mx-2 px-3 py-2 uppercase border-none bg-transparent text-white text-base ml-2 hover:shadow-lg hover:bg-cyan-600"
@@ -34,11 +38,6 @@ export const HomePage: React.FC<Props> = () => {
             </button>
           </Link>
         </div>
-        ) : (
-            <div className="flex items-center gap-[35px] absolute top-[10px] right-[10px] uppercase text-white text-[20px] mr-[50px] hover:shadow-xl">
-              <div className="w-[50px] h-auto"><img className="w-full" src="src\assets\images\pp.png" alt="pic" /></div>
-              <p>Jean michel</p>
-            </div>
         )}
         <div className="flex flex-col items-center">
           <img
@@ -48,12 +47,18 @@ export const HomePage: React.FC<Props> = () => {
           />
         </div>
         <div className="flex flex-col items-center">
-          <Link to={"/join_game"}>
-            <button className="font-font italic font-black w-[500px] h-[100px] bg-gradient-to-b from-orange to-orange-clear uppercase text-3xl m-5 cursor-pointer text-white rounded-lg hover:bg-gradient-to-r">
+          {accountService.isLoggedIn() ? (
+            <Link to={"/join_game"}>
+              <button className="font-font italic font-black w-[500px] h-[100px] bg-gradient-to-b from-orange to-orange-clear uppercase text-3xl m-5 cursor-pointer text-white rounded-lg hover:bg-gradient-to-r">
+                jouer
+              </button>
+            </Link>
+          ): (
+            <button className="font-font italic font-black w-[500px] h-[100px] bg-gradient-to-b from-orange to-orange-clear uppercase text-3xl m-5 cursor-pointer text-white rounded-lg hover:bg-gradient-to-r" onClick={() => setIsOpen(true)}>
               jouer
-            </button>
-            
-          </Link>
+            </button> 
+          )
+          }
           
           <Link to={"/parameter"}>
             <button
